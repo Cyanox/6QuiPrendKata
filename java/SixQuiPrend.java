a49c495855ad0b9160d857a1ac492d94d4ab38fa
Cyanox <alexis.moyse@gmail.com>
2021-03-05 20:03:54 +0100

    Number of BullHeads Finder

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -5,9 +5,20 @@ public class SixQuiPrend {
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
 
-        return 0;
+        if(value == 55 ) {
+            return 7;
+        }
+        else if (value%10 == 0 ){
+            return 3;
+        }
+        else if( (value%5 == 0 )&&( value%10 !=0 )) {
+            return 2;
+        }
+        else if( (value%11 == 0 )&&( value !=100 )) {
+            return 5;
+        } else
+            return 1;
     }
 
     public static void gameLoop() {
